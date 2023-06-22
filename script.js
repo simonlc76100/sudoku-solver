2e267362b042b1fced608bddbdd3b7b1b20c777b
simonlc76100 <simon.lellouche@edu.devinci.fr>
2023-06-22 23:35:28 +0200

    add getGridRows function

--- script.js
+++ script.js
@@ -12,8 +12,21 @@ function createGrid() {
   }
 }
 
+function getGridRows(cells) {
+  let rowsArray = new Array(9).fill().map(() => new Array(9));
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      rowsArray[i][j] = cells[i * 9 + j];
+    }
+  }
+  return rowsArray;
+}
+
 function main() {
   createGrid();
+
+  const cells = document.querySelectorAll('td[id^="cell_"]');
+  const rowsArray = getGridRows(cells);
 }
 
 document.addEventListener("DOMContentLoaded", main);
