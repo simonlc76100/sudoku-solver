725ba1617755f602829c64e51f74c288ff5c341c
simonlc76100 <simon.lellouche@edu.devinci.fr>
2023-06-22 18:08:30 +0200

    add main function and createGrid function

---
+++ script.js
@@ -0,0 +1,19 @@
+function createGrid() {
+  const grid = document.getElementById("sudokuGrid");
+  for (let i = 0; i < 9; i++) {
+    const row = grid.insertRow(i);
+    row.id = "row_" + i;
+    for (let j = 0; j < 9; j++) {
+      const cell = row.insertCell(j);
+      cell.id = "cell_" + i + "_" + j;
+      const input = document.createElement("input");
+      cell.appendChild(input);
+    }
+  }
+}
+
+function main() {
+  createGrid();
+}
+
+document.addEventListener("DOMContentLoaded", main);
