9fce3a0d55979189ade3968d1866a555a69f83c3
simonlc76100 <simon.lellouche@edu.devinci.fr>
2023-06-24 12:57:01 +0200

    add getGridMatrix function

--- script.js
+++ script.js
@@ -12,8 +12,25 @@ function createGrid() {
   }
 }
 
+function getGridMatrix(cells) {
+  let gridMatrix = new Array(9).fill().map(() => new Array(9).fill(0));
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (cells[i * 9 + j].firstChild.value !== "")
+        matrix[i][j] = parseInt(cells[i * 9 + j].firstChild.value);
+    }
+  }
+  return gridMatrix;
+}
+
 function main() {
   createGrid();
+
+  const cells = document.querySelectorAll('td[id^="cell_"]');
+
+  console.log(getGridMatrix(cells));
+
   const solveButton = document.getElementById("solveButton");
 }
 
